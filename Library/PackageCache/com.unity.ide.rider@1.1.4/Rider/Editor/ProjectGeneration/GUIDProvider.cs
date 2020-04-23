2aba75945132f392e0841dfda26afb3c9fe6bc3e
BelialJXK <belail.jiang@gmail.com>
2020-04-24 01:20:01 +0200

    first commit

--- Library/PackageCache/com.unity.ide.rider@1.1.4/Rider/Editor/ProjectGeneration/GUIDProvider.cs
+++ Library/PackageCache/com.unity.ide.rider@1.1.4/Rider/Editor/ProjectGeneration/GUIDProvider.cs
@@ -1,16 +1,3 @@
-using System;
-
-namespace Packages.Rider.Editor {
-  class GUIDProvider : IGUIDGenerator
-  {
-    public string ProjectGuid(string projectName, string assemblyName)
-    {
-      return SolutionGuidGenerator.GuidForProject(projectName + assemblyName);
-    }
-
-    public string SolutionGuid(string projectName, string extension)
-    {
-      return SolutionGuidGenerator.GuidForSolution(projectName, extension); // GetExtensionOfSourceFiles(assembly.sourceFiles)
-    }
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9edf6ea853881d2b75ee02c26912a08826c8fab483cd11859b2f8d5d8776df1c
+size 484
