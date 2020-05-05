19c06ec5561adb093d73878745a1ceffea3497ea
JamesMacroZhang <zhang1997226@qq.com>
2020-05-05 17:01:44 +0200

    Menu function
    Added Menu function to project.

---
+++ Assets/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Button Exit;
+    public Button ScanP;
+    public Button ScanW;
+    public Button Introduction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Exit.onClick.AddListener(EXIT);
+        ScanP.onClick.AddListener(SP);
+        ScanW.onClick.AddListener(SW);
+        Introduction.onClick.AddListener(IT);
+    }
+
+    private void IT()
+    {
+        SceneManager.LoadScene("Introduction");
+    }
+
+    private void SW()
+    {
+        SceneManager.LoadScene("TextRecognition");
+    }
+
+    private void SP()
+    {
+        SceneManager.LoadScene("ImageRecognition");
+    }
+
+    private void EXIT()
+    {
+        Debug.Log("exit");
+        Application.Quit();
+    }
+}
