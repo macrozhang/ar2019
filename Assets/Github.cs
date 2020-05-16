dad4690440c653b1f8def88023d4b6c527921c27
JamesMacroZhang <zhang1997226@qq.com>
2020-05-16 19:29:51 +0200

    Add Introduction page & GitHub skipping button.
    Add Introduction page.
    Add GitHub repository skipping button.

---
+++ Assets/Github.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Github : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button btn;
+    void Start()
+    {
+        btn.onClick.AddListener(GotoGitHub);
+    }
+
+    private void GotoGitHub()
+    {
+        Application.OpenURL("https://github.com/macrozhang/ar2019");
+    }
+}
+
